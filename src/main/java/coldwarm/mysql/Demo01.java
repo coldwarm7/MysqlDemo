3a035a2bef9e6b07d0299d0714982a92b90d7218
coldwarm <1259404192@qq.com>
2018-05-23 00:42:53 +0800

    first commit

---
+++ src/main/java/coldwarm/mysql/Demo01.java
@@ -0,0 +1,64 @@
+package coldwarm.mysql;
+
+
+import java.sql.*;
+
+/**
+ * Create by coldwarm on 2018/5/20.
+ */
+
+public class Demo01 {
+
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/testjdbc?autoReconnect=true&useSSL=true","root","123456");
+
+//        add(conn);
+
+//        delete(conn);
+
+//        select(conn);
+
+//        batch(conn);
+    }
+
+    public static void add(Connection coon) throws SQLException {
+        String sql = "insert into t_user (username,pwd) values (?,?)";
+        PreparedStatement ps = coon.prepareStatement(sql);
+        ps.setString(1, "coldwarm000");
+        ps.setString(2, "coldwarm111");
+        ps.execute();
+    }
+
+    public static void delete(Connection coon) throws SQLException {
+        String sql = "delete from t_user where id > ?";
+        PreparedStatement ps = coon.prepareStatement(sql);
+        ps.setObject(1,7);
+        ps.execute();
+    }
+
+    public static void select(Connection coon) throws SQLException {
+        String sql = "select id,username,pwd from t_user where id > ?";
+        PreparedStatement ps = coon.prepareStatement(sql);
+        ps.setObject(1,7);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            System.out.println(rs.getInt(1) + "---" +
+                    rs.getString(2) + "---" + rs.getString(3));
+        }
+    }
+
+    //批处理  最好使用statement
+    // PrepareStatement可能会导致预处理空间不足而报错
+    public static void batch(Connection coon) throws SQLException {
+        coon.setAutoCommit(false);   //手动提交
+
+        Statement statement = coon.createStatement();
+        for(int i = 0;i<10;i++){
+            statement.addBatch("insert into t_user (username, pwd, regTime) " +
+                    "values ('fz "+ i + " ' ,666,now())");
+        }
+        statement.executeBatch();
+        coon.commit();
+    }
+}
---
+++ src/main/java/coldwarm/mysql/Demo02.java
@@ -0,0 +1,47 @@
+package coldwarm.mysql;
+
+import java.sql.*;
+
+import static java.lang.System.*;
+
+/**
+ * Create by coldwarm on 2018/5/23.
+ */
+//sql事务测试
+
+public class Demo02 {
+    public static void main(String[] args) throws SQLException {
+        transaction();
+    }
+
+
+    public static void transaction() throws SQLException {
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/testjdbc?autoReconnect=true&useSSL=true","root","123456");
+        try {
+            String sql1 = "insert into t_user (username,pwd,regTime) values (?,?,?)";
+            PreparedStatement ps1 = conn.prepareStatement(sql1);
+            ps1.setString(1, "coldwarm000");
+            ps1.setString(2, "coldwarm111");
+            Timestamp timestamp1 = new Timestamp(System.currentTimeMillis()); //需要将long类型转换为Timestamp类型
+            ps1.setTimestamp(3, timestamp1);
+            ps1.execute();
+
+            String sql2 = "insert into t_user (username,pwd,regTime) values (?,?,?)";
+            PreparedStatement ps2 = conn.prepareStatement(sql2);
+            ps2.setString(1, "cishu");
+            ps2.setString(2, "coldwarm111");
+            Timestamp timestamp2 = new Timestamp(System.currentTimeMillis());
+            ps2.setTimestamp(3, timestamp2);
+            ps2.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            }catch (SQLException e1){
+                e1.printStackTrace();
+            }
+        }
+
+    }
+}
